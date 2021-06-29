ed8b15d226a63b34641ceac5c3632ce0504b9a6d
Liang <liang60711@gmail.com>
2021-06-29 20:36:49 +0800

    layouts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\CartComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +22,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', HomeComponent::class);
+Route::get('/shop', ShopComponent::class);
+Route::get('/checkout', CheckoutComponent::class);
+Route::get('/cart', CartComponent::class);
